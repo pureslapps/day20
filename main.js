4ea6ae661adec5bfc7206ce04800e302baba8ecd
Jorge Bhagwandeen <jorgebhagwandeen@yahoo.com>
2019-05-20 12:52:22 -0400

    commented out js page

--- main.js
+++ main.js
@@ -1,34 +1,33 @@
-    $(".result").html( data );
-});
-
-
-fetch('http://ecample.com/movies.json') then(funtion(response ) {
-
-})
-
-
-
-AJAX request method
-
-function loadDoc() {
-  var xhttp = new XMLHttpRequest(); // create an XHR object request object
-  xhttp.onreadystatechange = function() {
-    if (this.readyState == 4 && this.status == 200) {
-     document.getElementById("demo").innerHTML = this.responseText; // the response text is very improtant 
-    }
-  };
-  xhttp.open("GET", "ajax_info.txt", true); // open, then specify the get request, which the http (http verb, url, )
-  xhttp.send(); // invokes the XHR request,
-}
-
-loadDoc()
-
-class XMLHttpRequest() {
-  constructor() {
-
-  }
-
-onreadystatechange() {
-
-  }
-}
+//     $(".result").html( data );
+// });
+//
+//
+// fetch('http://ecample.com/movies.json') then(funtion(response ) {
+//
+// })
+//
+// class XMLHttpRequest() {
+//   constructor() {
+//
+//   }
+//
+//   onreadystatechange() {
+//
+//   }
+// }
+//
+//
+// AJAX request method
+//
+// function loadDoc() {
+//   var xhttp = new XMLHttpRequest(); // create an XHR object request object
+//   xhttp.onreadystatechange = function() {
+//     if (this.readyState == 4 && this.status == 200) {
+//      document.getElementById("demo").innerHTML = this.responseText; // the response text is very improtant
+//     }
+//   };
+//   xhttp.open("GET", "ajax_info.txt", true); // open, then specify the get request, which the http (http verb, url, )
+//   xhttp.send(); // invokes the XHR request,
+// }
+//
+// loadDoc()
